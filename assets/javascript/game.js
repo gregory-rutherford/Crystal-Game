d0ae5e9e81536602640904b8cdadb1ab12d605bb
Gregory Rutherford <gregorford@gmail.com>
2019-03-29 13:58:00 -0500

    styled, fixed a few pieces of logic, added wins and losses tally

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,82 @@
 var targetNumber = Math.floor(Math.random() * 120) + 19;
 var numberOptions = [1, 3, 7, 12];
 var scoreCounter = 0;
-console.log(targetNumber);
+var wins = 0;
+var losses = 0;
+
 $("#numberGuess").text(targetNumber);
 $("#currentTotal").text(scoreCounter);
-$.each(numberOptions, function(index, value) {
-    console.log(index, value);
+
+shuffle(numberOptions);
+crystalDom();
+crystalClick();
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+console.log(numberOptions);
+
+function crystalClick() {
+  $(".crystal-image").on("click", function() {
+    var crystalValue = $(this).attr("data-crystalvalue");
+    crystalValue = parseInt(crystalValue);
+    scoreCounter += crystalValue;
+    winLoss();
+  });
+}
+
+function crystalDom() {
+  $.each(numberOptions, function(index, value) {
+    // console.log(index, value);
     var crystalImage = $("<img>");
     crystalImage.addClass("crystal-image");
-    crystalImage.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    crystalImage.attr(
+      "src",
+      "https://png2.kisspng.com/sh/615c41794299fb4435ffc5f8a7a40683/L0KzQYm3VsMxN5p4gJH0aYP2gLBuTfNzgaR5ed42YXX2hLnslPlke15oiut8dHHvPcL8ggJ1gl46eqUDNEe1SIrsgcdiPl89UKs9NEmzR4K8U8A1OWU9UKoBNEm8PsH1h5==/kisspng-crystal-aesthetics-crystal-quartz-5b3847289ea7a6.8894490715304148886499.png"
+    );
     crystalImage.attr("data-crystalvalue", value);
     $("#crystals").append(crystalImage);
-});
-
-$(".crystal-image").on("click", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    
-    scoreCounter += crystalValue;
-
-    $("#currentTotal").text(scoreCounter);
-    if (scoreCounter === targetNumber) {
-      alert("You win!");
-    }
-    else if (scoreCounter >= targetNumber) {
-      alert("You lose!!");
-    }
-
   });
+}
+
+function winLoss() {
+  $("#currentTotal").text(scoreCounter);
+  if (scoreCounter === targetNumber) {
+    alert("You win!");
+    wins ++;
+    $("#wins").text("Wins: " + wins);
+    reset();
+  } else if (scoreCounter >= targetNumber) {
+    alert("You lose!!");
+    losses ++;
+    $("#losses").html("Losses: " + losses);
+    reset();
+  }
+}
+
+function reset() {
+  shuffle(numberOptions);
+  scoreCounter = 0;
+  targetNumber = Math.floor(Math.random() * 120) + 19;
+  $("#numberGuess").text(targetNumber);
+  console.log(targetNumber);
+  $("#currentTotal").text(scoreCounter);
+}
+//reset function
+// you need to reset the score counter, pick a new number and assign new values to each crystal
